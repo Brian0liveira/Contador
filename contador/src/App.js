a2c6f5f603fcafe5be5d8648b1514b9be62bd0d7
Brian0liveira <brian.twitter.2004@gmail.com>
2021-05-10 19:49:06 +0000

    Contador - Aula 3
    Signed-off-by: Brian0liveira <brian.twitter.2004@gmail.com>

---
+++ contador/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+import Contador from './components/Contador'
+import Card from './components/layout/Card';
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => (
+
+      <div className="App">
+          <h1> Contador </h1>
+
+          <div className="Cards" >
+
+              <Card titulo="Contador componentizado" color="gray">
+                  <Contador numeroInicial={10} />
+              </Card>
+
+          </div>
+      </div>
+
+);
---
+++ contador/src/components/Display.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props => {
+
+    return(
+        
+        <div>
+            <h2>{props.numero}</h2>
+        </div>
+
+    )
+}
